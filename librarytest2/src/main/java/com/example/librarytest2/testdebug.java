7ffa94ce0142105b90789cf7400b52e2d1671abb
shivammaindola <shivammaindola07@gmail.com>
2020-04-03 23:54:19 +0530

    Initial commit

---
+++ librarytest2/src/main/java/com/example/librarytest2/testdebug.java
@@ -0,0 +1,13 @@
+package com.example.librarytest2;
+
+import android.util.Log;
+
+public class testdebug {
+    public static final String Tag="sample_app";
+
+    public static void func(String message)
+    {
+        Log.d(Tag,message);
+    }
+
+}
